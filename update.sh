3b716fe3b461973aea524b3392e7e64a42f0aa2b
Dmitry Yakhnov <dmitry@yakhnov.info>
2019-05-09 10:58:12 +1000

    Easy rebuild and redeploy script

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Build
+docker build . -t mytardis/k8s-scp:latest
+
+# Push
+docker push mytardis/k8s-scp:latest
+
+# Update
+kubectl -n mytardis scale deployment.apps/scp --replicas=0
+sleep 3
+kubectl -n mytardis scale deployment.apps/scp --replicas=1
+
+# Watch
+watch kubectl -n mytardis get pods
