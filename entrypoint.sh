4b3eea283b62c7a5478475da5cbf988d88f1957b
Dmitry Yakhnov <dmitry@yakhnov.info>
2019-06-06 15:53:58 +1000

    Sync code updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-# This won't be executed if keys already exist (i.e. from a volume)
-ssh-keygen -A
+# Copy shared keys from S3 bucket
+cp /data/scp/ssh_host_* /etc/ssh/
+chmod 0600 /etc/ssh/ssh_host_*
 
-groupmod --non-unique --gid "$GROUPID" "$GROUP_NAME"
-usermod --non-unique --home "$DATADIR" --shell /usr/bin/rssh --uid "$USERID" --gid "$GROUPID" "$USER_NAME"
+if [ ! -f /etc/ssh/ssh_host_key ]; then
+  # This won't be executed if keys already exist
+  echo "Generating SSH keys..."
+  ssh-keygen -A
+  cp /etc/ssh/ssh_host_* /data/scp/
+fi
+
+groupmod --non-unique --gid "${GROUPID}" "${GROUP_NAME}"
+usermod --non-unique --home "${DATADIR}" --uid "${USERID}" --gid "${GROUPID}" "${USER_NAME}"
 
 # Copy authorized keys from ENV variable
-echo "$AUTHORIZED_KEYS" >$AUTHORIZED_KEYS_FILE
+echo "${AUTHORIZED_KEYS}" >$AUTHORIZED_KEYS_FILE
 chown "${USER_NAME}:${GROUP_NAME}" $AUTHORIZED_KEYS_FILE
 
 # Run sshd on container start
--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Build
-docker build . -t mytardis/k8s-scp:latest
+docker build . --squash -t mytardis/k8s-scp:latest
 
 # Push
 docker push mytardis/k8s-scp:latest
@@ -9,7 +9,7 @@ docker push mytardis/k8s-scp:latest
 # Update
 kubectl -n mytardis scale deployment.apps/scp --replicas=0
 sleep 3
-kubectl -n mytardis scale deployment.apps/scp --replicas=1
+kubectl -n mytardis scale deployment.apps/scp --replicas=2
 
 # Watch
 watch kubectl -n mytardis get pods
