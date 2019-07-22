80d8419c0f8024ca791d4bdd37ec8f9ed4440cde
James Wettenhall <james.wettenhall@gmail.com>
2019-07-22 21:54:28 +1000

    If AUTHORIZED_KEYS_SOURCE_FILE isn't supplied as a volume, just create an empty file (using "touch").

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,11 +9,11 @@ usermod --non-unique --uid "$USERID" --gid "$GROUPID" "$USER_NAME"
 # Authorized keys from scp-config should be volume mounted at
 # $AUTHORIZED_KEYS_SOURCE_FILE which defaults to /AUTHORIZED_KEYS
 # We'll make a copy, so we can change ownership and permissions:
-touch $AUTHORIZED_KEYS_TARGET_FILE
-if [[ -v AUTHORIZED_KEYS_SOURCE_FILE ]]
+if [[ ! -e "${AUTHORIZED_KEYS_SOURCE_FILE}" ]]
 then
-    cp $AUTHORIZED_KEYS_SOURCE_FILE $AUTHORIZED_KEYS_TARGET_FILE
+    touch $AUTHORIZED_KEYS_SOURCE_FILE
 fi
+cp $AUTHORIZED_KEYS_SOURCE_FILE $AUTHORIZED_KEYS_TARGET_FILE
 chown "${USER_NAME}:${GROUP_NAME}" $AUTHORIZED_KEYS_TARGET_FILE
 chmod 600 $AUTHORIZED_KEYS_TARGET_FILE
 
